bdc2c1dd5b7b05a313da639cde3604b464570da6
dshaw0004 <dipankarshaw692@gmail.com>
2025-06-10 20:14:26 +0530

    update: temporary update in routing
    when user click on any button it will directly take it to test page
    which is the old landing page. This is temporary until proper login is
    not implemented.

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,8 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/getstarted.tsx"),
-  route("home", "routes/home.tsx"),
+  route("home", "routes/test.tsx"),
+  route("homepage", "routes/home.tsx"),
   route("mocktest", "routes/mocktest.tsx"),
   route("instructions", "routes/instructions.tsx"),
   route("result", "routes/result.tsx"),
