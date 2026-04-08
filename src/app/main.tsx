de822b76147eeb147e8227fe432ef799393c7789
Dipankar Shaw <114005178+dshaw0004@users.noreply.github.com>
2026-04-08 06:54:53 +0000

    feat: implement routing with react-router and lazy loading for pages

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,14 +1,78 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+
+import { Suspense, lazy, type ReactElement } from "react";
+import { createRoot } from 'react-dom/client'
 // import './index.css'
-// import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Loader2 from "../components/loader/loader2";
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
+const MockTest = lazy(() => import("../pages/mockTest/mockTest"));
+const PreviousQuestions = lazy(() => import("../pages/previousQuestions"));
+const Result = lazy(() => import("../pages/result"));
+const StartTest = lazy(() => import("../pages/startTest"));
 
-console.log("Hello, World! This is the main entry point for the app.")
+const withLoader = (element: ReactElement) => (
+  <Suspense
+    fallback={
+      <div className="size-full flex justify-center align-center">
+        <Loader2 />
+      </div>
+    }
+  >
+    {element}
+  </Suspense>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/mocktest",
+    element: withLoader(<MockTest />),
+  },
+  {
+    path: "/previous-questions",
+    element: withLoader(<PreviousQuestions />),
+  },
+  {
+    path: "/result",
+    element: withLoader(<Result />),
+  },
+  {
+    path: "/start-test",
+    element: withLoader(<StartTest />),
+  },
+//   {
+//     path: "/app",
+//     element: <Outlet />,
+//     children: [
+//       {
+//         path: "mocktest",
+//         element: withLoader(<MockTest />),
+//       },
+//       {
+//         path: "previous-questions",
+//         element: withLoader(<PreviousQuestions />),
+//       },
+//       {
+//         path: "result",
+//         element: withLoader(<Result />),
+//       },
+//       {
+//         path: "start-test",
+//         element: withLoader(<StartTest />),
+//       },
+//     ],
+//   },
+], {
+    basename: "/app",
+});
+
+
+const root = document.getElementById("root");
+
+createRoot(root).render(
+  <RouterProvider router={router} />,
+);
